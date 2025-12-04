9357b8015e883dc0bcd22f86d4051d431c70b154
Marino Joseph <marinojoseph.lopez.bonifacio@gmail.com>
2025-12-04 10:22:11 -0500

    sumas

--- ejeciciosdeprueba/suma_par_impar.js
+++ ejeciciosdeprueba/suma_par_impar.js
@@ -1,2 +1,24 @@
 
-let respuesta = parseInt(prompt('Por favor escriba un numero'))
+
+let respuesta = parseInt(prompt('escriba un numero'))
+
+let numero = respuesta
+
+let suma = respuesta
+
+
+do {
+    if (suma % 2 > 0 && respuesta == 0) {
+        alert('la suma es ' + suma + ' impar.')
+
+        break
+    }
+    if (suma % 2 == 0 && respuesta == 0) {
+        alert('la suma es ' + suma + ' es par.')
+        numero = 0
+        suma = 0
+    }
+    respuesta = parseInt(prompt('escriba un numero'))
+    numero = numero + respuesta
+    suma = numero
+} while (respuesta >= 0)
