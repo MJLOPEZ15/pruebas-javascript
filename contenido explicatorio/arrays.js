ce2edde41f5489983e32ec0c779f1c6ed67241f3
Marino Joseph <marinojoseph.lopez.bonifacio@gmail.com>
2025-12-09 11:19:53 -0500

    extra

---
+++ contenido explicatorio/arrays.js
@@ -0,0 +1,9 @@
+//ejemplo
+//los arrays son los datos que  colocamos entre [] estos se usan para agrupar datos en ellos para luego poder llamarlos, ademas de crear diferentes agrupaciones
+
+const numbers = [1, 2, 3, 4, 5]
+
+numbers[0] = 10
+numbers[2] = 30
+
+console.log(numbers) // [10, 2, 30, 4, 5]
